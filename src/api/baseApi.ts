ee67451434d04115316f82917db990fc73cabdbc
Md yasen rafe <mdyasenrafe@gmail.com>
2024-09-02 01:55:39 +0600

    base api updated

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { RootState } from "../redux/store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://advance-uni-server.vercel.app/api/v1",
+  baseUrl: "https://rental-bike-assignment-3.vercel.app/api",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
     if (token) {
