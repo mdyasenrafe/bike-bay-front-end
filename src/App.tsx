baf98f10d9c280400d4afc4e29b1c1d3ef67ffad
Md yasen rafe <mdyasenrafe@gmail.com>
2024-09-02 01:34:02 +0600

    redux setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
+import { Provider } from "react-redux";
+import { store } from "./redux";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Provider store={store}>
+      <RouterProvider router={router} />{" "}
+    </Provider>
+  );
 }
 
 export default App;
