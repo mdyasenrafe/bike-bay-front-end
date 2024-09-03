a616e307edb9ff24d4612192d356633c0ca68cc0
Md yasen rafe <mdyasenrafe@gmail.com>
2024-09-03 16:27:58 +0600

    file structure updated

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -1,4 +1,12 @@
 import { Signin, Signup } from "../pages/auth";
+import {
+  BikeManagement,
+  CouponManagement,
+  ReturnBike,
+  UserManagement,
+} from "../pages/dashboard/admin";
+import { Profile } from "../pages/dashboard/shared";
+import { MyRental } from "../pages/dashboard/user";
 import { About, BikeDetail, Home } from "../pages/main";
 
 export type AppRoute = {
@@ -45,5 +53,70 @@ export const appRoutes: AppRoute[] = [
     path: "/bike-detail/:productId",
     component: <BikeDetail />,
     isNavItem: false,
+  },
+  {
+    id: 6,
+    name: "Admin Dashboard",
+    path: "/dashboard/admin",
+    isNavItem: false,
+    children: [
+      {
+        id: 6.1,
+        name: "Bike Management",
+        path: "/dashboard/admin/bike-management",
+        component: <BikeManagement />,
+        isNavItem: false,
+      },
+      {
+        id: 6.2,
+        name: "Coupon Management",
+        path: "/dashboard/admin/coupon-management",
+        component: <CouponManagement />,
+        isNavItem: false,
+      },
+      {
+        id: 6.3,
+        name: "Return Bike",
+        path: "/dashboard/admin/return-bike",
+        component: <ReturnBike />,
+        isNavItem: false,
+      },
+      {
+        id: 6.4,
+        name: "Profile",
+        path: "/dashboard/admin/profile",
+        component: <Profile />,
+        isNavItem: false,
+      },
+      {
+        id: 6.5,
+        name: "User Management",
+        path: "/dashboard/admin/user-management",
+        component: <UserManagement />,
+        isNavItem: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: "User Dashboard",
+    path: "/dashboard/user",
+    isNavItem: false,
+    children: [
+      {
+        id: 7.1,
+        name: "My Rental",
+        path: "/dashboard/user/my-rental",
+        component: <MyRental />,
+        isNavItem: false,
+      },
+      {
+        id: 7.2,
+        name: "Profile",
+        path: "/dashboard/user/profile",
+        component: <Profile />,
+        isNavItem: false,
+      },
+    ],
   },
 ];
