a9206a9615b2304bf1e889b602d9060cc12fbf0e
Md yasen rafe <mdyasenrafe@gmail.com>
2024-09-03 15:41:49 +0600

    dark mode added

--- src/components/layouts/MainLayout/MainLayout.tsx
+++ src/components/layouts/MainLayout/MainLayout.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { Navbar } from "../Navbar";
+import { getDarkMode } from "../../../redux/features/theme";
+import { useAppSelector } from "../../../redux";
 
 type MainLayoutProps = {
   children: React.ReactNode;
 };
 
 export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  const isDarkMode = useAppSelector(getDarkMode);
   return (
-    <React.Fragment>
+    <section className={`${isDarkMode ? "dark" : ""} `}>
       <Navbar />
-      <React.Fragment>{children}</React.Fragment>
-    </React.Fragment>
+      <React.Fragment>
+        <div>{children}</div>
+      </React.Fragment>
+    </section>
   );
 };
