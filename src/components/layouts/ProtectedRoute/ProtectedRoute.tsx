2c60e44dc941037dbbb9abe83e628874df0950ca
Md yasen rafe <mdyasenrafe@gmail.com>
2024-09-03 16:17:13 +0600

    protected route added

---
+++ src/components/layouts/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { verifyToken } from "../../../utils/verifyToken";
+import { TUser, logout, useCurrentToken } from "../../../redux/features/auth";
+import { useAppDispatch, useAppSelector } from "../../../redux";
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+  role: string | undefined;
+}
+
+export const ProtectedRoute: React.FC<ProtectedRouteProps> = ({
+  children,
+  role,
+}) => {
+  const token = useAppSelector(useCurrentToken);
+  const dispatch = useAppDispatch();
+  let user;
+
+  if (token) {
+    user = verifyToken(token) as TUser;
+  }
+
+  if (role && user?.role !== role) {
+    dispatch(logout());
+    return <Navigate to="/login" replace={true} />;
+  }
+
+  if (!token) {
+    return <Navigate to="/login" replace={true} />;
+  }
+
+  return <>{children}</>;
+};
