2062f7d7380f57dcfb9a00715aea08082a27a7d5
Md yasen rafe <mdyasenrafe@gmail.com>
2024-09-05 01:45:05 +0600

    fixed schema and base api header hting

--- src/Schema/BookingSchema.ts
+++ src/Schema/BookingSchema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const bookingSchema = z.object({
-  startDate: z.string().min(1, "Start Date is required"),
-  startTime: z.string().min(1, "Start Time is required"),
+  startDate: z.any(),
+  startTime: z.any(),
 });
