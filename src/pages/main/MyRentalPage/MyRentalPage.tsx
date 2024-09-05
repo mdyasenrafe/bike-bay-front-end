9b3d404709cb2a91ec83c6a1c566515066ff8670
Md yasen rafe <mdyasenrafe@gmail.com>
2024-09-06 02:01:16 +0600

    delete unnecessary folder

--- src/pages/main/MyRentalPage/MyRentalPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const MyRentalPage = () => {
-  return <div>MyRentalPage</div>;
-};
--- src/pages/main/index.ts
+++ src/pages/main/index.ts
@@ -2,5 +2,4 @@ export * from "./Home";
 export * from "./About";
 export * from "./BikeListing";
 export * from "./BikeDetail";
-export * from "./MyRentalPage";
 export * from "./PaymentSuccess";
