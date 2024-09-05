d894c2452dd5f26272290b3378001a2bf49200dc
Md yasen rafe <mdyasenrafe@gmail.com>
2024-09-06 04:38:14 +0600

    remove comments

--- src/Schema/BookingSchema.ts
+++ src/Schema/BookingSchema.ts
@@ -4,40 +4,35 @@ import { z } from "zod";
 export const bookingSchema = z
   .object({
     startDate: z.preprocess((arg) => {
-      // Convert Dayjs or string to a Date object
       if (typeof arg === "string" || arg instanceof Date) {
         return new Date(arg);
       } else if (arg && dayjs.isDayjs(arg)) {
-        return arg.toDate(); // Convert Dayjs to Date
+        return arg.toDate();
       }
       return undefined;
     }, z.date().min(dayjs().startOf("day").toDate(), { message: "Start date must be today or in the future" })),
 
     startTime: z.preprocess((arg) => {
-      // Convert Dayjs or string to a Date object
       if (typeof arg === "string" || arg instanceof Date) {
         return new Date(arg);
       } else if (arg && dayjs.isDayjs(arg)) {
-        return arg.toDate(); // Convert Dayjs to Date
+        return arg.toDate();
       }
       return undefined;
     }, z.date()),
   })
   .refine(
     (data) => {
-      // Validate startTime based on the selected startDate
       const startDate = dayjs(data.startDate);
       const startTime = dayjs(data.startTime);
 
       if (startDate.isSame(dayjs(), "day")) {
-        // If startDate is today, startTime must be in the future
         return startTime.isAfter(dayjs());
       }
-      // If startDate is in the future, startTime is valid anytime
       return true;
     },
     {
       message: "Start time must be in the future if the start date is today.",
-      path: ["startTime"], // Specify the path for the error message
+      path: ["startTime"],
     }
   );
