9f81798465db7aa00934b3b5f843e98bd4fab3a7
Md yasen rafe <mdyasenrafe@gmail.com>
2024-09-09 00:28:22 +0600

    deleted updated files

--- src/pages/dashboard/admin/UpdateProduct/UpdateProduct.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const UpdateProduct = () => {
-  return <div>UpdateProduct</div>;
-};
--- src/pages/dashboard/admin/index.ts
+++ src/pages/dashboard/admin/index.ts
@@ -1,6 +1,5 @@
 export * from "./BikeManagement";
 export * from "./CouponManagement";
-export * from "./UpdateProduct";
 export * from "./CreateProduct";
 export * from "./UserManagement";
 export * from "./RentalLists";
